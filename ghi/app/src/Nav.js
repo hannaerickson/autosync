ea55f343b4456327f987415b3baa504616833a6c
hanna <hmae221@gmail.com>
2022-12-12 10:04:06 -0600

    Added Nav functionality

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -2,85 +2,64 @@ import { NavLink } from 'react-router-dom';
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-info">
-      <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">Dorsey's Dealers</NavLink>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item dropdown">
-              <NavLink className="nav-link dropdown-toggle" to="#" id="navbarDropdownMenuLink" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sales</NavLink>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <NavLink className="dropdown-item bg-secondary" to="/sales/new">Record a Sale</NavLink>
-                <NavLink className="dropdown-item bg-secondary" to="/sales">All Sales</NavLink>
-              </div>
-            </li>
-            <li className="nav-item dropdown">
-              <NavLink className="nav-link dropdown-toggle" to="#" id="navbarDropdownMenuLink" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Service</NavLink>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <NavLink className="dropdown-item bg-secondary" to="/appointments/new">Schedule an Appointment</NavLink>
-                <NavLink className="dropdown-item bg-secondary" to="/appointments">All Appointments</NavLink>
-                <NavLink className="dropdown-item bg-secondary" to="/appointments/history">Appointment History</NavLink>
-              </div>
-            </li>
-            <li className="nav-item dropdown">
-              <NavLink className="nav-link dropdown-toggle" to="#" id="navbarDropdownMenuLink" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Inventory</NavLink>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <NavLink className="dropdown-item bg-secondary" to="/manufacturers">Manufacturers</NavLink>
-                <NavLink className="dropdown-item bg-secondary" to="/models">Models</NavLink>
-                <NavLink className="dropdown-item bg-secondary" to="/automobiles">Current Inventory</NavLink>
-              </div>
-            </li>
-            <li className="nav-item dropdown">
-              <NavLink className="nav-link dropdown-toggle" to="#" id="navbarDropdownMenuLink" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">People</NavLink>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <NavLink className="dropdown-item bg-secondary" to="/customers">Customers</NavLink>
-                <NavLink className="dropdown-item bg-secondary" to="/technicians">Technicians</NavLink>
-                <NavLink className="dropdown-item bg-secondary" to="/salesperson">Sales Representatives</NavLink>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  )
-}
-
-export default Nav;
 
 
-// THIS BELOW IS CORRECT NAV
 
-{/* <nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+<nav className="navbar navbar-expand-lg bg-success p-2 text-dark bg-opacity-25">
+  <div className="container-fluid">
+    <NavLink className="navbar-brand text-dark" to="/">Dorsey's Dealers</NavLink>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
+    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul className="navbar-nav">
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle text-dark" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Sales
           </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          <ul className="dropdown-menu">
+            <li><NavLink className="dropdown-item" to="/sales">All Sales</NavLink></li>
+            <li><NavLink className="dropdown-item" to="/sales/new">Register a Sale</NavLink></li>
+          </ul>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle text-dark" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Service
+          </a>
+          <ul className="dropdown-menu">
+            <li><NavLink className="dropdown-item" to="/appointments">All Service Appointments</NavLink></li>
+            <li><NavLink className="dropdown-item" to="/appointments/new">Schedule a Service Appointment</NavLink></li>
+            <li><NavLink className="dropdown-item" to="/appointments/history">Service History</NavLink></li>
+          </ul>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle text-dark" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Inventory
+          </a>
+          <ul className="dropdown-menu">
+            <li><NavLink className="dropdown-item" to="/manufacturers">Manufacturers</NavLink></li>
+            <li><NavLink className="dropdown-item" to="/models">Models</NavLink></li>
+            <li><NavLink className="dropdown-item" to="/automobiles">Current Inventory</NavLink></li>
+          </ul>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle text-dark" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            People
+          </a>
+          <ul className="dropdown-menu">
+            <li><NavLink className="dropdown-item" to="/salesperson">Sales Representatives</NavLink></li>
+            <li><NavLink className="dropdown-item" to="/technicians">Technicians</NavLink></li>
+            <li><NavLink className="dropdown-item" to="/customers">Customers</NavLink></li>
           </ul>
         </li>
       </ul>
     </div>
   </div>
-</nav> */}
+</nav>
+
+
+
+  )
+}
+
+export default Nav;
